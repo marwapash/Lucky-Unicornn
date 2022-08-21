2dfd429d8a38fbdc03192e08f0dda713217cc624
marwapash <108495855+marwapash@users.noreply.github.com>
2022-08-21 22:44:27 +1200

    Add files via upload

---
+++ how_much_v1.py
@@ -0,0 +1,26 @@
+# functions go here -----------------------------------------
+
+
+
+# Main routine goes here
+
+error = "Please enter a whole number between 1 and 10\n"
+
+valid = False
+while not valid:
+    try:
+        # Ask the question
+        response = int(input("How much would you like to play with? ")) 
+
+        # If the amounr is too low / too high give
+        if 0 < response <= 10:
+            print("You have asked to play with ${}".format(response))
+
+        # Output an error
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
+    
+
